5164a7e4b50ecc61d0523adeaf06c92148beda97
FionaBroderick <fiona.broderick@ericsson.com>
2019-08-13 14:29:45 +0100

    with back button

--- TrackMyHours/app/src/main/java/com/example/trackmyhours/Report.java
+++ TrackMyHours/app/src/main/java/com/example/trackmyhours/Report.java
@@ -2,10 +2,11 @@ package com.example.trackmyhours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Report extends AppCompatActivity {
-
 
 
     @Override
@@ -13,4 +14,10 @@ public class Report extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
     }
+
+    public void goToMainActivity (View view){
+        Intent intent = new Intent (this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
