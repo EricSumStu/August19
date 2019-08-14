f2141e178cc50ea837db86427270c327c9aff513
FionaBroderick <fiona.broderick@ericsson.com>
2019-08-14 14:42:26 +0100

    Start of textviews

--- TrackMyHours/app/src/main/java/com/example/trackmyhours/Report.java
+++ TrackMyHours/app/src/main/java/com/example/trackmyhours/Report.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Report extends AppCompatActivity {
 
@@ -13,6 +15,25 @@ public class Report extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        final int N = 10; // total number of textviews to add
+
+        final TextView[] myTextViews = new TextView[N]; // create an empty array;
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
+        for (int i = 0; i < N; i++) {
+            // create a new textview
+            final TextView rowTextView = new TextView(this);
+
+            // set some properties of rowTextView or something
+            rowTextView.setText("Date: 3rd Aug\n" + "Working hours: " + "00:00:00\n\n");
+
+            // add the textview to the linearlayout
+            linearLayout.addView(rowTextView);
+
+            // save a reference to the textview for later
+            myTextViews[i] = rowTextView;
+        }
     }
 
     public void goToMainActivity (View view){
