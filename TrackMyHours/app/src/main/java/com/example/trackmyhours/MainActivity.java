522aa08cd34c63eab131ed6c56c5498e6f83f151
jameshayesII <53428829+jameshayesII@users.noreply.github.com>
2019-08-14 16:38:54 +0100

    Added ability to read/write to file

--- TrackMyHours/app/src/main/java/com/example/trackmyhours/MainActivity.java
+++ TrackMyHours/app/src/main/java/com/example/trackmyhours/MainActivity.java
@@ -9,21 +9,27 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.logging.Logger;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+    public static final String FILE_NAME = "example.txt" ;
     private Chronometer chronometer;
     private long pauseOffset;
     private Button mButtonStart;
@@ -57,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
             chronometer.start();
             running = true;
-//
+            load(v);
             updateButtons();
 
         }
@@ -74,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void resetChronometer(View v) {
-        showElapsedTime();
+        String time = showElapsedTime();
         chronometer.setBase(SystemClock.elapsedRealtime());
         pauseOffset = 0;
 
@@ -82,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         running = false;
 
         updateButtons();
-
+        save(v, time );
     }
 
 
@@ -107,11 +113,62 @@ public class MainActivity extends AppCompatActivity {
                 Toast.LENGTH_LONG).show();
      return totalWorkingHours;
     }
+    public void save(View v, String elapsedTime) {
 
 
-    public void goToReport (View view){
+        try {
+            PrintWriter writer = new PrintWriter(new FileOutputStream(new File(getFilesDir() + "/" + "example.txt"), true));
+            String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+            String row = "Day " + date + " : [Working Hours: " + elapsedTime + "] " ;
+            writer.println(row);
+            writer.close();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + MainActivity.FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException");
+            e.printStackTrace();
+        }
+    }
+
+
+    public void load(View v) {
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+/*             textView = findViewById(R.id.reading);
+            textView.setText(sb.toString());*/
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+
+            public void goToReport (View view){
         Intent intent = new Intent (this, Report.class);
         startActivity(intent);
     }
+
 
 }
