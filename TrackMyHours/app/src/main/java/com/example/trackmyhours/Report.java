c7ecb8ae843024bb3e40128cb79b27500d1a4eb3
FionaBroderick <fiona.broderick@ericsson.com>
2019-08-16 13:54:17 +0100

    Table connected to spinner

--- TrackMyHours/app/src/main/java/com/example/trackmyhours/Report.java
+++ TrackMyHours/app/src/main/java/com/example/trackmyhours/Report.java
@@ -8,6 +8,7 @@ import android.text.Html;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -26,8 +27,24 @@ public class Report extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner2);
+        String spinnerValue = mySpinner.getSelectedItem().toString();
+        int spinnerValueInt = Integer.parseInt(spinnerValue);
+        readDataFromFile(spinnerValueInt);
+    }
 
+    public void reDrawTiles(View v){
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner2);
+        String spinnerValue = mySpinner.getSelectedItem().toString();
+        int spinnerValueInt = Integer.parseInt(spinnerValue);
+        readDataFromFile(spinnerValueInt);
+
+    }
+
+    private void readDataFromFile(int counter) {
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        linearLayout.removeAllViews();
         FileInputStream fis = null;
 
         try {
@@ -36,8 +53,9 @@ public class Report extends AppCompatActivity {
             BufferedReader br = new BufferedReader(isr);
             StringBuilder sb = new StringBuilder();
             String text;
+            int innerCounter = 0;
 
-            while ((text = br.readLine()) != null) {
+            while ((text = br.readLine()) != null && innerCounter < counter) {
                 final TextView rowTextView = new TextView(this);
                 rowTextView.setTextSize(20);
                 String[] textArray = text.split(" ");
@@ -48,6 +66,7 @@ public class Report extends AppCompatActivity {
                 linearLayout.addView(rowTextView);
 
                 sb.append(text).append("\n");
+                innerCounter++;
             }
 
 
